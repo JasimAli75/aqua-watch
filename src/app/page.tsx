1b1ad9df603352fc6374a79cd51ab4e3f4ff3914
Jasim Ali Rajput <115194580+JasimAli75@users.noreply.github.com>
2023-07-19 02:08:03 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <main className="font-semibold text-4xl text-gray-600">
       <div>new project Jasim Ali Rajput</div>
+      <p>jasima li new project </p>
     </main>
   );
 }
